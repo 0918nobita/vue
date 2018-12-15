825003fe3b7ea0bd540e0c9d4b677a6870501ec1
0918nobita <nobita.0918@gmail.com>
2018-12-15 19:27:19 +0900

    :sparkles: Store のサンプルを作成した

--- example/src/main.js
+++ example/src/main.js
@@ -1,10 +1,7 @@
-import 'babel-polyfill';
 import Vue from 'vue';
-import Vuex from 'vuex';
 import App from './App';
 
 Vue.config.productionTip = false;
-Vue.use(Vuex);
 
 /* eslint-disable no-new */
 new Vue({
